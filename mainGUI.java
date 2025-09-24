380192a3e3a7e046209013256cd1073167733c63
Sam Harvey <sam.harvey24@students.bhasvic.ac.uk>
2025-09-24 09:55:08 +0100

    Created main GUI structure

--- mainGUI.java
+++ mainGUI.java
@@ -1,2 +1,39 @@
+import javax.swing.*;
+
 public class mainGUI {
+    private JPanel mainPanel;
+    private JComboBox toolsComboBox;
+    private JComboBox effectsComboBox;
+    private JPanel playbackPanel;
+    private JButton rewindButton;
+    private JButton playButton;
+    private JButton pauseButton;
+    private JLabel timeLabel;
+    private JPanel northPanel;
+    private JPanel timelinePanel;
+    private JPanel southPanel;
+    private JButton instrument1;
+    private JButton instrument2;
+    private JButton instrument3;
+    private JButton instrument4;
+    private JButton instrument5;
+    private JPanel instrument1Timeline;
+    private JPanel instrument2Timeline;
+    private JPanel instrument3Timeline;
+    private JPanel instrument4Timeline;
+    private JPanel instrument5Timeline;
+    private JProgressBar timelineProgressBar;
+    private JLabel instrument1Label;
+    private JLabel instrument2Label;
+    private JLabel instrument3Label;
+    private JLabel instrument4Label;
+    private JLabel instrument5Label;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("mainGUI");
+        frame.setContentPane(new mainGUI().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
